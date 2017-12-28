cdb1ca5e38772df490e2776164319a7c4502db04
rohithpv23 <rvalappil@dresources.com>
2017-12-28 20:34:03 +0530

    Added files.

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ import express from 'express';
 import path from 'path';
 import handlebars from 'express-handlebars';
 import routes from './app/routes/main.js';
+import parser from 'body-parser';
 
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
+
+app.use((req, res, next) => {
+  res.setHeader('X-Powered-By', 'Rohith Auth App');
+  next();
+});
 
 app.use('/', express.static(path.join(__dirname, 'public'), {
   setHeaders: function (res, path, stat) {
@@ -22,17 +28,33 @@ app.engine('hbs', handlebars({
 }));
 app.set('view engine', 'hbs');
 
-// Middlewares
-app.use(routes());
+// Application Middleware
+app
+  .use(parser.json())
+  .use(parser.urlencoded({
+    extended: true,
+  }));
 
-app.use((req, res, next) => {
-  res.setHeader('X-Powered-By', 'Rohith Auth App');
-  next();
-});
+  // Middlewares
+  app.use(routes());
 
 // POST requests
 app.post("/login", function (req, res) {
   res.send('Logged In...');
+});
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+// define as the last app.use callback
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send(err.message);
 });
 
 app.listen(app.get('port'), () => console.log('Listening on port 3000!'));
