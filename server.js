6e5461b21f2ecba3709b814e2d5b04cb26325ee7
rohithpv23 <rvalappil@dresources.com>
2017-12-29 19:01:04 +0530

    Added files.

--- server.js
+++ server.js
@@ -4,10 +4,31 @@ import path from 'path';
 import handlebars from 'express-handlebars';
 import routes from './app/routes/main.js';
 import parser from 'body-parser';
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
+const SALT_WORK_FACTOR = 10;
+
+//connect to MongoDB
+mongoose.connect('mongodb://localhost/auth');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log("MongoDB connected!");
+});
+
+
+// Schema for users collection.
+const userSchema = mongoose.Schema({
+  username: { type: String, required: true, index: { unique: true } },
+  password: { type: String, required: true }
+});
+
+// Create collection 'users'.
+const User = mongoose.model('Users', userSchema);
 
 app.use((req, res, next) => {
   res.setHeader('X-Powered-By', 'Rohith Auth App');
@@ -35,12 +56,20 @@ app
     extended: true,
   }));
 
-  // Middlewares
-  app.use(routes());
+// Middlewares
+app.use(routes());
 
 // POST requests
-app.post("/login", function (req, res) {
-  res.send('Logged In...');
+app.post("/login", (req, res) => {
+  // Create a record of type users and save.
+  const testUser = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  testUser.save((err, doc) => {
+    if(err) res.json(err);
+    else res.send('Logged In...');
+  });
 });
 
 // catch 404 and forward to error handler
